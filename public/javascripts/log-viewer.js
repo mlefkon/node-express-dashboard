39d852d77832ef3ab610c9bc763dd9b5982e1182
Marc Lefkon <marc@leftek.com>
2023-02-22 16:06:03 +0800

    Completed module

---
+++ public/javascripts/log-viewer.js
@@ -0,0 +1,13 @@
+console.log("Log-Viewer init...");
+
+const connection = new WebSocket("ws://localhost:3000");
+const logWindow = document.getElementById('log-window');
+const filePath = document.getElementById('logFilePath');
+
+connection.onopen = (connection) => { if (filePath) connection.send(filePath); }
+connection.onmessage = (event) => {
+    const logs = event.data.split("\n").join("<hr>");
+    logWindow.innerHTML = logs; // event.data;
+}
+connection.onclose = (connection) => {connection.send("bye bye.")}
+console.log("Log-Viewer setup complete.");
